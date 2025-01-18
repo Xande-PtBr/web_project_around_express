a5767609786957629b889042e599b44cc0e31dd7
Alexandre Silva <xande.xas@gmail.com>
2025-01-18 21:50:35 +0000

    mudando a rota cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,8 @@
 const { Router } = require("express");
-const fs = require("fs");
-const path = require("path");
+const cards = require("../data/cards.json");
 const router = new Router();
 
 router.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "../data/cards.json");
-  const cards = fs.readFileSync(filePath, "utf-8");
   return (
     res.status(200).json(cards),
     function (err, cards) {
